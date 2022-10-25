609c024b83eccae1ee29fec7abffd04ea401f409
Biki Gurung <bikigurung8@gmail.com>
2022-10-24 23:44:51 -0400

    Changed Terraform File

--- intTerraform/main.tf
+++ intTerraform/main.tf
@@ -34,14 +34,14 @@ module "vpc" {
   tags = var.vpc_tags
 }
 
-module "web_server_sg" {
-  source              = "terraform-aws-modules/security-group/aws"
-  name                = "web-server"
-  description         = "Security group for web-server"
-  vpc_id              = module.vpc.vpc_id
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp"]
-}
+# module "web_server_sg" {
+#   source              = "terraform-aws-modules/security-group/aws"
+#   name                = "web-server"
+#   description         = "Security group for web-server"
+#   vpc_id              = module.vpc.vpc_id
+#   ingress_cidr_blocks = ["0.0.0.0/0"]
+#   ingress_rules       = ["http-80-tcp"]
+# }
 
 
 
